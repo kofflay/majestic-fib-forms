a288349d8ad11cddb58e9f60000df7e390a4eea8
kofflay <blagov03124@gmail.com>
2026-08-03 17:35:55 +0300

    Create [...nextauth].js

---
+++ majestic-fib/pages/api/auth/[...nextauth].js
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+export const authOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "identify guilds", // Без этого /users/@me будет возвращать 401
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Сохраняем access_token из Discord в JWT токен сессии
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Пробрасываем токен в объект session, чтобы использовать его в API
+      if (token.accessToken) {
+        session.accessToken = token.accessToken;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };

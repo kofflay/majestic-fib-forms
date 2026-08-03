bc68adea2ad8340e8315fd2472aab94785b4d7f3
kofflay <blagov03124@gmail.com>
2026-08-03 14:04:53 +0300

    Create login.js

---
+++ majestic-fib/pages/api/auth/login.js
@@ -0,0 +1,10 @@
+// pages/api/auth/login.js
+export default (req, res) => {
+  const redirectUri = `\${process.env.VERCEL_URL || 'http://localhost:3000'}/api/auth/callback`;
+  const scope = 'identify';
+  const responseType = 'code';
+  
+  const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=${responseType}&scope=${scope}`;
+  
+  res.redirect(authUrl);
+};

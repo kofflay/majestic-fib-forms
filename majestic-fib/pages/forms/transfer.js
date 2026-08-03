27ec83b133425034c1fc37679a7982f4cf54d076
kofflay <blagov03124@gmail.com>
2026-08-03 18:28:44 +0300

    Update transfer.js

--- majestic-fib/pages/forms/transfer.js
+++ majestic-fib/pages/forms/transfer.js
@@ -1,1 +1,13 @@
 
+import { useRouter } from 'next/router';
+
+export default function Transfer() {
+  const router = useRouter();
+  
+  return (
+    <div>
+      <h1>Перевод в отдел</h1>
+      <button onClick={() => router.push('/dashboard')}>Назад</button>
+    </div>
+  );
+}

46dd18a542da8ade5d84666c34775e7281c7c21a
kofflay <blagov03124@gmail.com>
2026-08-03 18:28:53 +0300

    Update report.js

--- majestic-fib/pages/forms/report.js
+++ majestic-fib/pages/forms/report.js
@@ -1,1 +1,12 @@
+import { useRouter } from 'next/router';
 
+export default function Report() {
+  const router = useRouter();
+  
+  return (
+    <div>
+      <h1>Отчёт о повышении</h1>
+      <button onClick={() => router.push('/dashboard')}>Назад</button>
+    </div>
+  );
+}

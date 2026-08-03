e58b077e0c2ad3f5d78f473986b03308ff4e6888
kofflay <blagov03124@gmail.com>
2026-08-03 18:20:45 +0300

    Update next.config.js

--- majestic-fib/next.config.js
+++ majestic-fib/next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['cdn.discordapp.com'],
-  },
-  // Публичные переменные, которые можно показывать в коде
-  publicRuntimeConfig: {
-    discordClientId: process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID,
-    discordRedirectUri: process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URI,
-  },
 }
 
 module.exports = nextConfig
